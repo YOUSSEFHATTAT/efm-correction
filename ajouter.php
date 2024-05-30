9bf54281883523b25690f0fae62dccbf54f2e200
YOUSSEFHATTAT <hattatyoussef6@gmail.com>
2024-05-30 22:25:24 +0100

    ajouter.php

--- ajouter.php
+++ ajouter.php
@@ -1,43 +1,42 @@
 <!DOCTYPE html>
-<html >
+<html lang="en">
 <head>
-    
-    <title>Document</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ajouter un client</title>
 </head>
 <body>
-    <form method="post">
-        <label>CIN</label>
-        <input type="text" name="cin"><br>
-        <label>Nom </label>
-        <input type="text" name="nom"><br>
-        <label>Prenom</label>
-        <input type="text" name="prenom"><br>
-        <label>Email</label>
-        <input type="text" name="email"><br>
-        <label>Password</label>
-        <input type="text" name="password"><br>
-        <input type="submit" value="ajouter">
-    </form>
-    <?php
-    try{
-        $pdo= new PDO('mysql:host=localhost,dbname=immobilier','root','');
-    }
-    catch(exeption $e){
-        echo 'erreur'.' ' echo $e->getmessage();
-    }
-    if(!empty($_POST['cin']) && !empty($_POST['nom']) && !empty($_POST['prenom']) && !empty($_POST['email'])&& !empty($_POST['password'])){
-        $cin=htmlspecialchars($_POST['cin']);
-        $nom= htmlspecialchars($_POST['nom']);
-        $prenom= htmlspecialchars($_POST['prenom']);
-        $email= htmlspecialchars($_POST['email']);
-        $password= htmlspecialchars($_POST['password']);
-        $sqlstate=$pdo->prepare('INSERT INTO client VALUES( ?,?,?,?,?)');
-        $sqlstate->execute([$$cin,$prenom,$email,$password]);
-    }
-    else{
-        echo 'tous les champs sont obligatoires.' ;
-    }
+    <?php 
+        if(isset($_POST['ajouter'])){
+            $cin = $_POST['cin'];
+            $nom = $_POST['nom'];
+            $prenom = $_POST['prenom'];
+            $email = $_POST['email'];
+            $password = $_POST['password'];
+            if(!empty($cin) && !empty($nom) && !empty($prenom) && !empty($email) && !empty($password) ){
+                //Insertion
+                $pdo = new PDO('mysql:host=localhost;dbname=immobilier','root','');
+                $sqlState = $pdo->prepare('INSERT INTO client VALUES(null,?,?,?,?,?)');
+                $sqlState->execute([$cin,$nom,$prenom,$email,$password]);
+                header('location: listerc.php');
+            }else{
+                echo "Tous les champs sont requis.";
+            }
+        }
     ?>
-        
+    <form method="post">
+        <label>CIN</label><br>
+        <input type="text" name="cin"><br>
+        <label>Nom</label><br>
+        <input type="text" name="nom"><br>
+        <label>Prenom</label><br>
+        <input type="text" name="prenom"><br>
+        <label>Email</label><br>
+        <input type="email" name="email"><br>
+        <label>Password</label><br>
+        <input type="password" name="password"><br><br>
+        <input type="submit" value="Ajouter" name="ajouter">
+    </form>
 </body>
 </html>
